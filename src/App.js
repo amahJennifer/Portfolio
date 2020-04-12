0fc8ffb5b76481cda388d16eaddd00d23cfa0a6b
amahJennifer <jennifer000@rocketmail.com>
2020-04-12 06:18:05 +0100

    Add css and Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import FirstSection from "./Components/firstSection/FirstSection"
 import './App.css';
+import AboutSection from './Components/aboutSection/AboutSection';
 
 function App() {
   return (
     <div className="App">
       <FirstSection />
+      <AboutSection />
     </div>
   );
 }
---
+++ src/Components/aboutSection/AboutSection.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import linkedin from "../../assests/images/linkedin.svg"
+import facebook from "../../assests/images/facebook.svg"
+import behance from "../../assests/images/behance.svg"
+import instagram from "../../assests/images/instagram.svg"
+import skype from "../../assests/images/skype.svg"
+import twitter from "../../assests/images/twitter.svg"
+import whatsapp from "../../assests/images/whatsapp.svg"
+
+import "./aboutsection.css"
+const AboutSection = () => {
+  return (
+    <div className="aboutSection">
+      <div className="aboutContainer">
+        <div className="little">
+          A little Bit 
+        </div>
+        <div className="aboutMe">About ME</div>
+        <div className="text">
+          <p>Jennifer Amah is a FullStack developer from Nigeria,She studied Computer Science And
+            Management with first class degree from Wisconsin International University College</p>
+          <p>Over the years she has worked on several live projects to solve real life problems ,Most of these Projects are fintech Apps .
+          Jennifer is most excited about fintech applications ,making and using purchasing power at the comfort of your bedroom
+          </p>
+          <p>Over the years she has worked on several live projects to solve real life problems ,Most of these Projects are fintech Apps .
+          Jennifer is most excited about fintech applications ,making and using purchasing power at the comfort of your bedroom
+          </p>
+        </div>
+        <div className="socialContainer">
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jennifer-amah-aa2949122/">
+              <img src={linkedin} className="social-icon"/>
+            </a>
+          </div>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jennifer-amah-aa2949122/">
+              <img src={facebook} className="social-icon" />
+            </a>
+          </div>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jennifer-amah-aa2949122/">
+              <img src={skype} className="social-icon" />
+            </a>
+          </div>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jennifer-amah-aa2949122/">
+              <img src={behance} className="social-icon" />
+            </a>
+          </div>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jennifer-amah-aa2949122/">
+              <img src={twitter} className="social-icon" />
+            </a>
+          </div>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jennifer-amah-aa2949122/">
+              <img src={instagram} className="social-icon" />
+            </a>
+          </div>
+          <div className="social">
+            <a href="https://www.linkedin.com/in/jennifer-amah-aa2949122/">
+              <img src={whatsapp} className="social-icon" />
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutSection
