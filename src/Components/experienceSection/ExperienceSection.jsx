4b01c3fa0c650b8e9c7367f0f97b4d5c49bc1fea
amahJennifer <jennifer000@rocketmail.com>
2020-04-12 10:42:17 +0100

    Add Job Section

---
+++ src/Components/experienceSection/ExperienceSection.jsx
@@ -0,0 +1,141 @@
+import React from 'react'
+import "./expSection.css"
+const ExperienceSection = () => {
+  return (
+		<div className="exp-Section animated fadeInRight delay-3s">
+			<div className="exp-container">
+				<div className="sub-title">The Companies I've Worked with</div>
+				<div className="title">Experience</div>
+				<div className="job-subSection">
+					<div className="job-company">Decagon</div>
+					<div className="job-position">FullStack Software Engineer</div>
+					<div className="job-date">2019-present</div>
+					<div className="job-details">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+							reprehenderit.
+						</p>
+						<div className="view">
+							<a href="#">View my interview about this role </a>
+							<a href="#">Visit Decagon's</a>
+						</div>
+					</div>
+				</div>
+				<div className="job-subSection">
+					<div className="job-company">Decagon</div>
+					<div className="job-position">FullStack Software Engineer</div>
+					<div className="job-date">2019-present</div>
+					<div className="job-details">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+							reprehenderit.
+						</p>
+						<div className="view">
+							<a href="#">View my interview about this role </a>
+							<a href="#">Visit Decagon's</a>
+						</div>
+					</div>
+				</div>
+				<div className="job-subSection">
+					<div className="job-company">Decagon</div>
+					<div className="job-position">FullStack Software Engineer</div>
+					<div className="job-date">2019-present</div>
+					<div className="job-details">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+							reprehenderit.
+						</p>
+						<div className="view">
+							<a href="#">View my interview about this role </a>
+							<a href="#">Visit Decagon's</a>
+						</div>
+					</div>
+				</div>
+				<div className="job-subSection">
+					<div className="job-company">Decagon</div>
+					<div className="job-position">FullStack Software Engineer</div>
+					<div className="job-date">2019-present</div>
+					<div className="job-details">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+							reprehenderit.
+						</p>
+						<div className="view">
+							<a href="#">View my interview about this role </a>
+							<a href="#">Visit Decagon's</a>
+						</div>
+					</div>
+				</div>
+			
+				
+			
+			</div>
+		</div>
+	);
+}
+
+export default ExperienceSection
