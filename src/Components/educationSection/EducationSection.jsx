f7490eac55e87d53b14988fc70d39987f4451699
amahJennifer <jennifer000@rocketmail.com>
2020-04-12 12:59:24 +0100

    Add Education Section And Animation

---
+++ src/Components/educationSection/EducationSection.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import "./edu.css"
+const EducationSection = () => {
+  return (
+		<div className="animated fadeInUp delay-4s edu-section">
+			<div className="edu-container">
+				<div className="sub-title">Things I've Learnt</div>
+				<div className="title">Education</div>
+				<div className="job-subSection">
+					<div className="job-company">
+						Wisconsin International University College
+					</div>
+					<div className="job-position-edu">
+						Bachelor of Arts with First Class Honours
+					</div>
+					<div className="job-position-edu">
+						Computer Science and Management
+					</div>
+					<div className="job-date">Sept 2012 - Nov 2016</div>
+					<div className="job-details-edu">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+							reprehenderit.
+						</p>
+					</div>
+				</div>
+				<div className="job-subSection">
+					<div className="job-company">Decagon Learning Institute</div>
+					<div className="job-position-edu">Software Enginnering</div>
+					<div className="job-position-edu">Javascript(Node.js,React.js)</div>
+					<div className="job-date">Sept 2019 - Feb 2020</div>
+					<div className="job-details-edu">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+							reprehenderit.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut
+							reprehenderit.
+						</p>
+					</div>
+				</div>
+				<div className="requestBtn">
+					<a href=" https://wa.me/2348148547405" className="requestLink">
+						Request My Full Resume
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default EducationSection
